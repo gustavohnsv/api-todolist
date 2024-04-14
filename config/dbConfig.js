7d234744d0231fa430603d15b8d81ee3fc509bb4
gustavohnsv <guvi10vieira@gmail.com>
2024-04-14 16:57:10 -0300

    fix: api_url request

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,11 +1,19 @@
 const mongoose = require('mongoose');
-const dbConfig = process.env.MONGODB_URI;
+
+if (!process.env.API_URL || process.env.API_URL.trim() === '') {
+  console.error('A variável de ambiente API_URL não está definida.');
+  process.exit(1); 
+}
+
+const dbConfig = process.env.API_URL;
 
 mongoose.connect(dbConfig, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
   .then(() => console.log('Conexão com o banco de dados estabelecida com sucesso'))
-  .catch(err => console.error('Erro ao conectar ao banco de dados:', err));
+  .catch(err => {
+    console.error('Erro ao conectar ao banco de dados:', err);
+  });
 
 module.exports = mongoose.connection;
