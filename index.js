6586bc0877a9211d07b0ff78d35dcaedc7330cf6
gustavohnsv <guvi10vieira@gmail.com>
2024-04-14 17:05:29 -0300

    feat: add port file

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const routes = require('./routes');
 const cors  = require('cors');
+const { port } = require('./port');
 const app = express();
 require('./config/dbConfig');
-
-const port = process.env.PORT || 3001;
 
 app.use(cors());
 app.use(express.json());
