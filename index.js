ad0e53e465a38d364407b2d2f6ec24f4590a98e5
gustavohnsv <guvi10vieira@gmail.com>
2024-04-14 12:52:59 -0300

    feat: add vercel scripts

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const cors  = require('cors');
 const app = express();
 require('./config/dbConfig');
 
+const port = process.env.PORT || 3001;
+
 app.use(cors());
 app.use(express.json());
 app.use(routes);
