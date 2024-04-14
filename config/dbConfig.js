8c968ffa44bc0d5d7813441c704c77cf67a806d3
gustavohnsv <guvi10vieira@gmail.com>
2024-04-14 14:22:29 -0300

    fix: dbconfig file

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,5 +1,11 @@
-const moongose = require('mongoose');
+const mongoose = require('mongoose');
 const dbConfig = process.env.MONGODB_URI;
-const connect = moongose.connect(dbConfig)
 
-module.exports = connect;
+mongoose.connect(dbConfig, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(() => console.log('Conexão com o banco de dados estabelecida com sucesso'))
+  .catch(err => console.error('Erro ao conectar ao banco de dados:', err));
+
+module.exports = mongoose.connection;
